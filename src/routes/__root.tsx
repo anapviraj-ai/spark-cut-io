454b4da78bf3c6c98949c931791f7b67efc33da1
Viraj anap <anapviraj@gmail.com>
2026-09-09 20:06:50 +0530

    feat: add unified image uploader with drag-and-drop, clipboard paste, and AI matting

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,7 +13,6 @@ import appCss from "../styles.css?url";
 import { Toaster } from "@/components/ui/sonner";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 
-
 function NotFoundComponent() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background px-4">
@@ -140,4 +139,3 @@ function RootComponent() {
     </QueryClientProvider>
   );
 }
-
